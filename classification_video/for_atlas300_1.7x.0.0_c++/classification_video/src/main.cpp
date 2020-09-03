b64fad0541fc10f880d87a845c653b2fb8b54e20
HuaweiAscend <ascend@huawei.com>
2020-09-03 09:56:50 +0800

    update classification_video/for_atlas300_1.7x.0.0_c++/classification_video/src/main.cpp.
    Former-commit-id: 0822c399a3aa8d632c34159430aeb17af2d326ef

--- classification_video/for_atlas300_1.7x.0.0_c++/classification_video/src/main.cpp
+++ classification_video/for_atlas300_1.7x.0.0_c++/classification_video/src/main.cpp
@@ -32,51 +32,51 @@ const char* kModelPath = "../model/googlenet.om";
 }
 
 int main(int argc, char *argv[]) {
-    //检查应用程序执行时的输入,程序执行的参数为输入视频文件路径
+    //Check the input when the application is executed, the parameter of the program execution is the input video file path
     if((argc < 2) || (argv[1] == nullptr)){
         ERROR_LOG("Please input: ./main <image_dir>");
         return FAILED;
     }
-    //实例化分类推理对象,参数为分类模型路径,模型输入要求的宽和高
+    //Instantiate the classification reasoning object, the parameter is the classification model path, the width and height of the model input requirements
     ClassifyProcess classify(kModelPath, kModelWidth, kModelHeight);
-    //初始化分类推理的acl资源, 加载模型和申请推理输入使用的内存
+    //Initialize the acl resource for classification reasoning, load the model and apply for the memory used for reasoning input
     Result ret = classify.Init();
     if (ret != SUCCESS) {
         ERROR_LOG("Classification Init resource failed");
         return FAILED;
     }
 
-    //使用opencv打开视频文件
+    //Use opencv to open the video file
     string videoFile = string(argv[1]);
     cv::VideoCapture capture(videoFile);
     if (!capture.isOpened()) {
         cout << "Movie open Error" << endl;
         return FAILED;
     }
-    //逐帧推理
+    //Frame-by-frame reasoning
     while(1) {
-        //读取一帧图片
+        //Read a picture
         cv::Mat frame;
         if (!capture.read(frame))
         {
             INFO_LOG("Video capture return false");
             break;
         }
-        //对帧图片进行预处理
+        //Preprocess the frame picture
         Result ret = classify.Preprocess(frame);
         if (ret != SUCCESS) {
             ERROR_LOG("Read file %s failed, continue to read next",
                       videoFile.c_str());
             continue;
         }
-        //将预处理的图片送入模型推理,并获取推理结果
+        //Send the preprocessed pictures to the model for inference and get the inference results
         aclmdlDataset* inferenceOutput = nullptr;
         ret = classify.Inference(inferenceOutput);
         if ((ret != SUCCESS) || (inferenceOutput == nullptr)) {
             ERROR_LOG("Inference model inference output data failed");
             return FAILED;
         }
-        //解析推理输出,并将推理得到的物体类别,置信度和图片送到presenter server显示
+        //Analyze the inference output, and send the object category, confidence and picture obtained by inference to the presenter server for display
         ret = classify.Postprocess(frame, inferenceOutput);
         if (ret != SUCCESS) {
             ERROR_LOG("Process model inference output data failed");
