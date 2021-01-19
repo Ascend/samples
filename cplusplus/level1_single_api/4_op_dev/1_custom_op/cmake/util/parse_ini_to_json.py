fc81e29e2da21277b98796cbd79da1d1cf3f9299
TEMUQILETU <jahyeonbeak@163.com>
2021-01-19 09:24:41 +0800

    脚本静态检查错误

--- cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/parse_ini_to_json.py
+++ cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/parse_ini_to_json.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
 import json
 import os
 import stat
@@ -5,18 +19,17 @@ import sys
 
 tbe_ops = {}
 
-
 def parse_ini_files(ini_files):
     tbe_ops_info = {}
     for ini_file in ini_files:
         parse_ini_to_obj(ini_file, tbe_ops_info)
     return tbe_ops_info
 
-
 def parse_ini_to_obj(ini_file, tbe_ops_info):
     with open(ini_file) as ini_file:
         lines = ini_file.readlines()
         op = {}
+        op_name = ""
         for line in lines:
             line = line.rstrip()
             if line.startswith("["):
@@ -29,12 +42,13 @@ def parse_ini_to_obj(ini_file, tbe_ops_info):
                 key1_0, key1_1 = key1.split(".")
                 if not key1_0 in op:
                     op[key1_0] = {}
+                if key1_1 in op[key1_0]:
+                  raise RuntimeError("Op:" + op_name + " " + key1_0 + " " + key1_1 + " is repeated!")
                 op[key1_0][key1_1] = key2
 
-
-def check_op_info(tbe_ops_info):
-    print("==============check valid for ops info start==============")
-    not_valid_op = []
+def check_op_info(tbe_ops):
+    print("\n\n==============check valid for ops info start==============")
+    not_valid_op=[]
     required_op_input_info_keys = ["paramType", "name"]
     required_op_output_info_keys = ["paramType", "name"]
     shape_type_valid_value = ["fix", "range", "list"]
@@ -42,8 +56,8 @@ def check_op_info(tbe_ops_info):
     param_type_valid_value = ["dynamic", "optional", "required"]
     required_attr_key = ["type", "value", "paramType"]
     is_valid = True
-    for op_key in tbe_ops_info:
-        op = tbe_ops_info[op_key]
+    for op_key in tbe_ops:
+        op = tbe_ops[op_key]
 
         for op_info_key in op:
             if op_info_key.startswith("input"):
@@ -53,11 +67,11 @@ def check_op_info(tbe_ops_info):
                     if not required_op_input_info_key in op_input_info:
                         missing_keys.append(required_op_input_info_key)
                 if len(missing_keys) > 0:
-                    print("op: " + op_key + " " + op_info_key + " missing: " + ",".join(missing_keys))
+                    print("op: " + op_key + " " + op_info_key +" missing: " + ",".join(missing_keys))
                     is_valid = False
                 else:
                     if not op_input_info["paramType"] in param_type_valid_value:
-                        print("op: " + op_key + " " + op_info_key + " paramType not valid, valid key:[dynamic, optional, required]")
+                        print("op: " + op_key + " " + op_info_key +" paramType not valid, valid key:[dynamic, optional, required]")
                         is_valid = False
             if op_info_key.startswith("output"):
                 op_input_info = op[op_info_key]
@@ -66,15 +80,14 @@ def check_op_info(tbe_ops_info):
                     if not required_op_input_info_key in op_input_info:
                         missing_keys.append(required_op_input_info_key)
                 if len(missing_keys) > 0:
-                    print("op: " + op_key + " " + op_info_key + " missing: " + ",".join(missing_keys))
+                    print("op: " + op_key + " " + op_info_key +" missing: " + ",".join(missing_keys))
                     is_valid = False
                 else:
                     if not op_input_info["paramType"] in param_type_valid_value:
-                        print("op: " + op_key + " " + op_info_key + " paramType not valid, valid key:[fix, range, list]")
+                        print("op: " + op_key + " " + op_info_key +" paramType not valid, valid key:[fix, range, list]")
                         is_valid = False
-    print("==============check valid for ops info end================\n")
+    print("==============check valid for ops info end================\n\n")
     return is_valid
-
 
 def write_json_file(tbe_ops_info, json_file_path):
     json_file_real_path = os.path.realpath(json_file_path)
@@ -82,8 +95,7 @@ def write_json_file(tbe_ops_info, json_file_path):
         # Only the owner and group have rights
         os.chmod(json_file_real_path, stat.S_IWGRP + stat.S_IWUSR + stat.S_IRGRP + stat.S_IRUSR)
         json.dump(tbe_ops_info, f, sort_keys=True, indent=4, separators=(',', ':'))
-    print("Compile tbe op info cfg successfully.")
-
+    print("Compile op info cfg successfully.")
 
 def parse_ini_to_json(ini_file_paths, outfile_path):
     tbe_ops_info = parse_ini_files(ini_file_paths)
@@ -93,24 +105,22 @@ def parse_ini_to_json(ini_file_paths, outfile_path):
     write_json_file(tbe_ops_info, outfile_path)
     return True
 
-
 if __name__ == '__main__':
     args = sys.argv
 
-    outfile_path = "tbe_ops_info.json"
-    ini_file_paths = []
+    output_file_path = "tbe_ops_info.json"
+    ini_file_path_list = []
 
     for arg in args:
         if arg.endswith("ini"):
-            ini_file_paths.append(arg)
+            ini_file_path_list.append(arg)
+            output_file_path = arg.replace(".ini", ".json")
         if arg.endswith("json"):
-            outfile_path = arg
+            output_file_path = arg
 
-    if len(ini_file_paths) == 0:
-        ini_file_paths.append("tbe_ops_info.ini")
+    if len(ini_file_path_list) == 0:
+        ini_file_path_list.append("tbe_ops_info.ini")
 
-    if not parse_ini_to_json(ini_file_paths, outfile_path):
+    if not parse_ini_to_json(ini_file_path_list, output_file_path):
         sys.exit(1)
     sys.exit(0)
-
-
