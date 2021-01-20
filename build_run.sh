0cd18eb3eaf9659568c2c52d0ce9783fc4ef7131
fuyangchenghu <1015138540@qq.com>
2021-01-20 16:30:17 +0800

    上传run包构建脚本

---
+++ build_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+install_scripts(){
+    lines=7
+    tail +$lines $0 >${HOME}/samples.tar.gz
+    tar jxvf ${HOME}/samples.tar.gz
+    rm ${HOME}/samples.tar.gz
+    exit 0
+}
+
+tar jcvf ${HOME}/samples.tar.bz2 ../samples
+sed -n "1p;3,7p" ./${0}  > ./install.sh
+sed 's/^[ \t]*//g' ./install.sh > soft_install.sh
+cat ./soft_install.sh ${HOME}/samples.tar.bz2 > ${HOME}/samples.run
+rm ./install.sh ./soft_install.sh ${HOME}/samples.tar.bz2
+
+
