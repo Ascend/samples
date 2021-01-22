8d879b3c29672b9c818817c6a11ce4ad5c9ddc3a
刘超越 <466015832@qq.com>
2021-01-22 11:07:01 +0800

    update python/common/atlas_utils/utils.py.

--- python/common/atlas_utils/utils.py
+++ python/common/atlas_utils/utils.py
@@ -1,7 +1,6 @@
 import acl
 from atlas_utils.constants import *
-from functools import wraps
-DEBUG = False
+
 from atlas_utils.acl_logger import log_error, log_info
 def check_ret(message, ret_int):
     if ret_int != 0:
@@ -98,16 +97,3 @@ def align_up2(value):
 def yuv420sp_size(width, height):
     return int(width * height * 3 / 2)   
 
-def display_time(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if DEBUG:
-            btime = time.time()
-            res = func(*args, **kwargs)
-            use_time = time.time() - btime
-            print("in %s, use time:%s" % (func.__name__, use_time))
-            return res
-        else:
-            return func(*args, **kwargs)
-
-    return wrapper
