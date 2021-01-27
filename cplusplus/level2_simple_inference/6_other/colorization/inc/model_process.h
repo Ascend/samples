bcc0a71fe346c47685b6f84661f7f6b440af4d59
wuyan <1136014326@qq.com>
2021-01-27 20:08:44 +0800

    update cplusplus/level2_simple_inference/6_other/colorization/inc/model_process.h.

--- cplusplus/level2_simple_inference/6_other/colorization/inc/model_process.h
+++ cplusplus/level2_simple_inference/6_other/colorization/inc/model_process.h
@@ -68,6 +68,12 @@ public:
     Result CreateInput(void *inputDataBuffer, size_t bufferSize);
 
     /**
+   * @get Model Size
+   */
+
+    size_t GetModelSize();
+
+    /**
     * @brief destroy input resource
     */
     void DestroyInput();
