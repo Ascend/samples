e473101b07e9a47e004d85ba459244f5c92c1d04
maodoumi <137259307@qq.com>
2021-01-29 10:32:22 +0800

    add blur2sharp examples

---
+++ cplusplus/level2_simple_inference/6_other/DeblurGAN_GOPRO_Blur2Sharp/src/main.cpp
@@ -0,0 +1,98 @@
+/**
+* Copyright 2020 Huawei Technologies Co., Ltd
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+
+* http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+
+* File main.cpp
+* Description: dvpp sample main func
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <dirent.h>
+
+#include <stdio.h>
+#include <unistd.h>
+#include "business_imp.h"
+#include "utils.h"
+using namespace std;
+
+namespace {
+uint32_t kModelWidth = 1280;
+uint32_t kModelHeight = 720;
+const char* kModelPath = "../model/blurtosharp_pad_1280_720.om";
+}
+
+int main(int argc, char *argv[]) {
+    //check the input, need the picture directory.
+    if((argc < 2) || (argv[1] == nullptr)){
+        ERROR_LOG("Please input: ./main <image_dir>");
+        return FAILED;
+    }
+
+    // according argv[0], get the work directory.
+    char work_file[200];
+    INFO_LOG("argv[0]:%s", argv[0]);
+    
+    realpath(argv[0], work_file);
+    // INFO_LOG("work_path = %s", work_file); 
+    string work_file_tmp = work_file;
+
+    string work_path = work_file_tmp.substr(0, work_file_tmp.find_last_of("/"));
+    INFO_LOG("work_path = %s", work_path.c_str()); 
+       
+
+    //Business implementation, need model path, model input image widht and height
+    BusinessImp businessImp(kModelPath, kModelWidth, kModelHeight, work_path);
+    //Initialization of ACL resource, model and memory
+    Result ret = businessImp.Init();
+    if (ret != SUCCESS) {
+        ERROR_LOG("Classification Init resource failed");
+        return FAILED;
+    }
+    //Get all the image file names in the image directory
+    string inputImageDir = string(argv[1]);
+    vector<string> fileVec;
+    Utils::GetAllFiles(inputImageDir, fileVec);
+    if (fileVec.empty()) {
+        ERROR_LOG("Failed to deal all empty path=%s.", inputImageDir.c_str());
+        return FAILED;
+    }
+    
+    for (string imageFile : fileVec) {
+        //Preprocessing image: read the image and zoom it to the size required by the model input
+        Result ret = businessImp.Preprocess(imageFile);
+        if (ret != SUCCESS) {
+            ERROR_LOG("Read file %s failed, continue to read next",
+                      imageFile.c_str());                
+            continue;
+        }
+        //The preprocessed images are sent to the model for inference
+        aclmdlDataset* inferenceOutput = nullptr;  
+        ret = businessImp.Inference(inferenceOutput);
+        if ((ret != SUCCESS) || (inferenceOutput == nullptr)) {
+            ERROR_LOG("Inference model inference output data failed");
+            return FAILED;
+        }
+
+        //Get inference output, and resize to the original size.
+        ret = businessImp.Postprocess(imageFile, inferenceOutput);
+        if (ret != SUCCESS) {
+            ERROR_LOG("Process model inference output data failed");
+            return FAILED;
+        }
+    }
+
+    INFO_LOG("Execute sample success");
+    return SUCCESS;
+}
