e6fcc3ab3f0725871a1c68d9c8e2571ae559cc34
Tixuan <tx1110@126.com>
2021-02-03 17:11:42 +0800

    update python/common/atlas_utils/acl_resource.py.

--- python/common/atlas_utils/acl_resource.py
+++ python/common/atlas_utils/acl_resource.py
@@ -1,9 +1,18 @@
+"""
+Copyright (R) @huawei.com, all rights reserved
+-*- coding:utf-8 -*-
+CREATED:  2021-01-20 20:12:13
+MODIFIED: 2021-02-03 14:04:45
+"""
 import acl
 
-from utils import *
+import atlas_utils.utils as utils
 from atlas_utils.resource_list import resource_list
 
 class AclResource(object):
+    """
+    AclResource
+    """
     def __init__(self, device_id=0):
         self.device_id = device_id
         self.context = None
@@ -11,21 +20,24 @@ class AclResource(object):
         self.run_mode = None
 
     def init(self):
+        """
+        init resource
+        """
         print("init resource stage:")
         ret = acl.init()
-        check_ret("acl.rt.set_device", ret)
+        utils.check_ret("acl.rt.set_device", ret)
 
         ret = acl.rt.set_device(self.device_id)
-        check_ret("acl.rt.set_device", ret)
+        utils.check_ret("acl.rt.set_device", ret)
 
         self.context, ret = acl.rt.create_context(self.device_id)
-        check_ret("acl.rt.create_context", ret)
+        utils.check_ret("acl.rt.create_context", ret)
 
         self.stream, ret = acl.rt.create_stream()
-        check_ret("acl.rt.create_stream", ret)
+        utils.check_ret("acl.rt.create_stream", ret)
 
         self.run_mode, ret = acl.rt.get_run_mode()
-        check_ret("acl.rt.get_run_mode", ret)
+        utils.check_ret("acl.rt.get_run_mode", ret)
 
         print("Init resource success")
 
