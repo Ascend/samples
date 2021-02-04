5d322e60c8ac53c28fc5ef83f0b6d8764fed505d
caoliang0601 <leoncao61@163.com>
2021-02-04 11:00:02 +0000

    add image_HDR_enhance

---
+++ python/contrib/image_HDR_enhance/scripts/testcase_300.sh
@@ -0,0 +1,168 @@
+tensorflow_model="https://modelzoo-train-atc.obs.cn-north-4.myhuaweicloud.com/003_Atc_Models/AE/ATC%20Model/image_HDR_enhance/image_HDR_enhance.pb"
+model_name="image_HDR_enhance"
+
+version=$1
+
+data_source="https://c7xcode.obs.cn-north-4.myhuaweicloud.com/models/image_HDR_enhance/"
+verify_source="https://c7xcode.obs.cn-north-4.myhuaweicloud.com/models/image_HDR_enhance/"
+project_name="image_HDR_enhance"
+
+script_path="$( cd "$(dirname $BASH_SOURCE)" ; pwd -P)"
+project_path=${script_path}/..
+
+declare -i success=0
+declare -i inferenceError=1
+declare -i verifyResError=2
+
+
+function downloadDataWithVerifySource() {
+
+    mkdir -p ${project_path}/data/
+
+    wget -O ${project_path}/data/"data0.png"  ${data_source}"data0.png"  --no-check-certificate
+    if [ $? -ne 0 ];then
+        echo "download tesdata0t1.png failed, please check Network."
+        return 1
+    fi
+
+    wget -O ${project_path}/data/"data1.png"  ${data_source}"data1.png"  --no-check-certificate
+    if [ $? -ne 0 ];then
+        echo "download test2.png failed, please check Network."
+        return 1
+    fi
+
+    mkdir -p ${project_path}/verify_image/
+
+    wget -O ${project_path}/verify_image/verify0.png ${verify_source}"verify0.png" --no-check-certificate
+    if [ $? -ne 0 ];then
+        echo "download verify0.png failed, please check Network."
+        return 1
+    fi
+
+    wget -O ${project_path}/verify_image/verify1.png ${verify_source}"verify1.png" --no-check-certificate
+    if [ $? -ne 0 ];then
+        echo "download verify1.png failed, please check Network."
+        return 1
+    fi
+
+    return 0
+}
+
+
+function setAtcEnv() {
+    # 设置模型转换时需要的环境变量
+    if [[ ${version} = "c73" ]] || [[ ${version} = "C73" ]];then
+        export install_path=/home/HwHiAiUser/Ascend/ascend-toolkit/latest
+        export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
+        export PYTHONPATH=${install_path}/atc/python/site-packages/te:${install_path}/atc/python/site-packages/topi:$PYTHONPATH
+        export ASCEND_OPP_PATH=${install_path}/opp
+        export LD_LIBRARY_PATH=${install_path}/atc/lib64:${LD_LIBRARY_PATH}
+    elif [[ ${version} = "c75" ]] || [[ ${version} = "C75" ]];then
+        export install_path=$HOME/Ascend/ascend-toolkit/latest
+        export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
+        export ASCEND_OPP_PATH=${install_path}/opp
+        export PYTHONPATH=${install_path}/atc/python/site-packages:${install_path}/atc/python/site-packages/auto_tune.egg/auto_tune:${install_path}/atc/python/site-packages/schedule_search.egg:$PYTHONPATH
+        export LD_LIBRARY_PATH=${install_path}/atc/lib64:${LD_LIBRARY_PATH}
+    fi
+
+    return 0
+}
+
+function downloadOriginalModel() {
+
+    mkdir -p ${project_path}/model/
+
+    wget -O ${project_path}/model/${tensorflow_model##*/} ${tensorflow_model} --no-check-certificate
+    if [ $? -ne 0 ];then
+        echo "install tensorflow_model failed, please check Network."
+        return 1
+    fi
+
+    return 0
+}
+
+function main() {
+
+    if [[ ${version}"x" = "x" ]];then
+        echo "ERROR: version is invalid"
+        return ${inferenceError}
+    fi
+
+    # 下载测试集和验证集
+    downloadDataWithVerifySource
+    if [ $? -ne 0 ];then
+        echo "ERROR: download test images or verify images failed"
+        return ${inferenceError}
+    fi
+
+    mkdir -p ${HOME}/models/${project_name}     
+    if [[ $(find ${HOME}/models/${project_name} -name ${model_name}".om")"x" = "x" ]];then 
+        # 下载原始模型文件[aipp_cfg文件]
+        downloadOriginalModel
+        if [ $? -ne 0 ];then
+            echo "ERROR: download original model failed"
+            return ${inferenceError}
+        fi
+
+        # 设置模型转换的环境变量
+        setAtcEnv
+        if [ $? -ne 0 ];then
+            echo "ERROR: set atc environment failed"
+            return ${inferenceError}
+        fi
+
+        # 转模型
+        cd ${project_path}/model/
+        atc --model=${project_path}/model/${tensorflow_model##*/} --framework=3 --output=${HOME}/models/${project_name}/${model_name} --soc_version=Ascend310  --input_shape="input:1,512,512,3" --input_format=NHWC --output_type=FP32
+        if [ $? -ne 0 ];then
+            echo "ERROR: convert model failed"
+            return ${inferenceError}
+        fi
+
+        ln -s ${HOME}/models/${project_name}/${model_name}".om" ${project_path}/model/${model_name}".om"
+        if [ $? -ne 0 ];then
+            echo "ERROR: failed to set model soft connection"
+            return ${inferenceError}
+        fi
+    else 
+        ln -s ${HOME}/models/${project_name}/${model_name}".om" ${project_path}/model/${model_name}".om"
+        if [ $? -ne 0 ];then
+            echo "ERROR: failed to set model soft connection"
+            return ${inferenceError}
+        fi
+    fi
+
+    cd ${project_path}/src
+
+    # 重新配置程序运行所需的环境变量
+    export LD_LIBRARY_PATH=
+    export LD_LIBRARY_PATH=/home/HwHiAiUser/Ascend/nnrt/latest/acllib/lib64:/home/HwHiAiUser/ascend_ddk/x86/lib:${LD_LIBRARY_PATH}
+    export PYTHONPATH=/home/HwHiAiUser/Ascend/nnrt/latest/pyACL/python/site-packages/acl:${PYTHONPATH}
+
+    # 运行程序
+    python3.6 main.py
+    if [ $? -ne 0 ];then
+        echo "ERROR: run failed. please check your project"
+        return ${inferenceError}
+    fi   
+    
+    # 调用python脚本判断本工程推理结果是否正常
+    for outimage in $(find ${project_path}/verify_image -name "*.png");do
+        tmp=`basename $outimage`
+        if [[ ! -d "${project_path}/output" ]];then
+            echo "ERROR: not find results folders!"
+            return ${verifyResError}
+        fi
+        for test_file in `find ${project_path}/output -name "*${tmp#*_}"`;do
+            python3 ${script_path}/verify_result.py ${test_file} ${outimage}
+            if [ $? -ne 0 ];then
+                echo "ERROR: The result of reasoning is wrong!"
+                return ${verifyResError}
+            fi   
+        done
+    done
+
+    echo "run success"
+    return ${success}
+}
+main
