50a9862f640a13fa5f3e5532e591f970ffcddc17
zhujianpeng <kdywta@163.com>
2021-02-07 19:48:47 -0800

    gesture_recognition

---
+++ python/contrib/gesture_recognition_picture/src/gesture_categories.py
@@ -0,0 +1,29 @@
+gesture_categories = [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'left',
+        'ok',
+        'right',
+        'rock',
+        'finger heart',
+        'praise',
+        'prayer',
+        'stop',
+        'Give the middle finger',
+        'bow',
+        'No gesture'
+]
+
+def get_gesture_categories(id):
+    if id >= len(gesture_categories):
+        return "unknown"
+    else:
+        return gesture_categories[id]
