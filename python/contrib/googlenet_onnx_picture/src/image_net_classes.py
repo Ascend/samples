cf6ff98c627c3f1867de1c474e28587b371e490f
zhujianpeng <kdywta@163.com>
2021-03-05 19:04:35 +0800

    update python/contrib/googlenet_onnx_picture/src/image_net_classes.py.

--- python/contrib/googlenet_onnx_picture/src/image_net_classes.py
+++ python/contrib/googlenet_onnx_picture/src/image_net_classes.py
@@ -1004,11 +1004,11 @@ image_net_classes = [
 "toilet tissue, toilet paper, bathroom tissue"
 ]
 
-def get_image_net_class(id):
+def get_image_net_class(image_id):
     """
 	get_gesture_categories
     """
-    if id >= len(image_net_classes):
+    if image_id >= len(image_net_classes):
         return "unknown"
     else:
-        return image_net_classes[id]
+        return image_net_classes[image_id]
