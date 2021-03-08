b9d7d2e292c0bebb8af9f686f4ce6d5b6bee59a5
caoliang <leoncao61@163.com>
2021-03-08 19:28:54 +0800

    update python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py.

--- python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py
+++ python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py
@@ -1007,9 +1007,9 @@ image_net_classes = [
 ]
 
 def get_image_net_class(id):
-"""
-get_image_net_class
-"""
+    """
+    get_image_net_class
+    """
     if id >= len(image_net_classes):
         return "unknown"
     else:
