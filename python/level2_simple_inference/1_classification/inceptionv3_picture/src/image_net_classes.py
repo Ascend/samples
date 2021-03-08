0cf35bedbc3324c47e78440c657861ec04ecc131
caoliang <leoncao61@163.com>
2021-03-08 19:33:05 +0800

    update python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py.

--- python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py
+++ python/level2_simple_inference/1_classification/inceptionv3_picture/src/image_net_classes.py
@@ -1006,11 +1006,11 @@ image_net_classes = [
 "toilet tissue, toilet paper, bathroom tissue"
 ]
 
-def get_image_net_class(id):
+def get_image_net_class(identifier):
     """
     get_image_net_class
     """
-    if id >= len(image_net_classes):
+    if identifier >= len(image_net_classes):
         return "unknown"
     else:
-        return image_net_classes[id]
+        return image_net_classes[identifier]
