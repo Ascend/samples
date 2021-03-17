9b70f5bb80723344a2cf013062bd6ed7523a7211
JiangFeng11 <45545092@qq.com>
2021-03-17 18:00:04 +0800

    add 3Dgesture

--- python/contrib/3Dgesture_recognition/src/3Dgesture_recognition.py
+++ python/contrib/3Dgesture_recognition/src/3Dgesture_recognition.py
@@ -1,3 +1,9 @@
+"""
+Copyright (R) @huawei.com, all rights reserved
+-*- coding:utf-8 -*-
+CREATED:  2021-02-02 09:44:13
+MODIFIED: 2021-02-22 09:44:13
+"""
 #!/usr/bin/env python
 # encoding: utf-8
 import sys
@@ -25,17 +31,23 @@ MODEL_PATH = os.path.join(SRC_PATH, "../model/3d_gesture_recognition.om")
 MODEL_WIDTH = 112
 MODEL_HEIGHT = 112
 image_net_classes = [
-   "Action1", "Action2","Action3", "Action4","Action5", "Action6","Action7","Action8",
+   "Action1", "Action2", "Action3", "Action4", "Action5", "Action6", "Action7", "Action8",
      "Action9", "Action10"]
 
 def get_image_net_class(class_id):
+    """
+    get_image_net_class
+    """
     if class_id >= len(image_net_classes):
         return "unknown"
     else:
         return image_net_classes[class_id]
 
 
-def post_process(infer_output,data_file):
+def post_process(infer_output, data_file):
+    """
+    post_process
+    """
     print("post process")
     data = infer_output[0]
     vals = data.flatten()
@@ -45,16 +57,20 @@ def post_process(infer_output,data_file):
         object_class = get_image_net_class(n)
         print("label:%d  confidence: %f, class: %s" % (n, vals[n], object_class))
     
-    (filepath,tempfilename) = os.path.split(data_file)
-    (filename,extension) = os.path.splitext(tempfilename)
+    (filepath, tempfilename) = os.path.split(data_file)
+    (filename, extension) = os.path.splitext(tempfilename)
     object_class = get_image_net_class(top_k[0])
-    output_path = os.path.join(os.path.join(SRC_PATH, "../outputs"), filename+".txt")
+    output_path = os.path.join(os.path.join(SRC_PATH, "../outputs"), filename + ".txt")
     with open(output_path, "w", encoding="utf-8") as fp:
         fp.write(object_class)
        
     return 
 
+
 def main():    
+    """
+    main
+    """
     if (len(sys.argv) != 2):
         print("The App arg is invalid")
         exit(1)
@@ -73,8 +89,8 @@ def main():
         os.mkdir(os.path.join(SRC_PATH, "../outputs"))
 
     for data_file in data_list:     
-        data_raw = np.fromfile(data_file,dtype =np.float32)
-        input_data =  data_raw.reshape(16,MODEL_WIDTH,MODEL_HEIGHT,3).copy()
+        data_raw = np.fromfile(data_file, dtype = np.float32)
+        input_data =  data_raw.reshape(16, MODEL_WIDTH, MODEL_HEIGHT, 3).copy()
         result = model.execute([input_data,])  
         post_process(result, data_file)    
 
--- python/contrib/3Dgesture_recognition/video2bin/bin2imgs.py
+++ python/contrib/3Dgesture_recognition/video2bin/bin2imgs.py
@@ -1,4 +1,4 @@
-## bin to images test
+""" bin to images test"""
 
 import cv2
 import numpy as np
@@ -22,6 +22,6 @@ imgArray.shape=1, CLIP_LENGTH, CROP_SIZE, CROP_SIZE, CHANNEL_NUM
 for i in range(CLIP_LENGTH):
     img = imgArray[0][i]
     #print(img.shape)
-    cv2.imwrite('./OutputImages/test'+str(i)+'.jpg', img.astype(int))
+    cv2.imwrite('./OutputImages/test' + str(i) + '.jpg', img.astype(int))
 
 
