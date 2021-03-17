28e21e0f23be2dab782740c8f90661cf6dfefb92
HuaweiAscend <ascend@huawei.com>
2021-03-17 15:53:39 +0800

    update build_run.sh.

--- build_run.sh
+++ build_run.sh
@@ -2,15 +2,15 @@
 install_scripts(){
     lines=7
     tail -n +$lines $0 >${HOME}/samples.tar.gz
-    tar jxvf ${HOME}/samples.tar.gz
+    tar jxvf ${HOME}/samples.tar.gz -C ${HOME}
     rm ${HOME}/samples.tar.gz
     exit 0
 }
 
-tar jcvf ${HOME}/samples.tar.bz2 ../samples
+tar jcvf ../samples.tar.bz2 ../samples
 sed -n "1p;3,7p" ./${0}  > ./install.sh
 sed 's/^[ \t]*//g' ./install.sh > soft_install.sh
-cat ./soft_install.sh ${HOME}/samples.tar.bz2 > ${HOME}/samples.run
-rm ./install.sh ./soft_install.sh ${HOME}/samples.tar.bz2
+cat ./soft_install.sh ../samples.tar.bz2 > ./samples.run
+rm ./install.sh ./soft_install.sh ../samples.tar.bz2
 
 
