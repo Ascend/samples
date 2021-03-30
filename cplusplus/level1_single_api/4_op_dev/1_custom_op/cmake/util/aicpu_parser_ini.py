0b7d7b640c1d7865fff889961b48d3341d724799
zhangxin <guoyue12@hisilicon.com>
2021-03-30 19:42:15 +0800

    update to canndev

--- cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/aicpu_parser_ini.py
+++ cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/aicpu_parser_ini.py
@@ -21,9 +21,9 @@ import stat
 import sys
 
 def parse_ini_files(ini_files):
-    """
+    '''
     init all ini files
-    """
+    '''
     aicpu_ops_info = {}
     for ini_file in ini_files:
         parse_ini_to_obj(ini_file, aicpu_ops_info)
@@ -31,33 +31,33 @@ def parse_ini_files(ini_files):
 
 
 def parse_ini_to_obj(ini_file, aicpu_ops_info):
-    """
+    '''
     parse all ini files to object
-    """
-    with open(ini_file) as ini_file:
-        lines = ini_file.readlines()
-        op = {}
+    '''
+    with open(ini_file) as ini_read_file:
+        lines = ini_read_file.readlines()
+        ops = {}
         for line in lines:
             line = line.rstrip()
             if line.startswith("["):
                 op_name = line[1:-1]
-                op = {}
-                aicpu_ops_info[op_name] = op
+                ops = {}
+                aicpu_ops_info[op_name] = ops
             else:
                 key1 = line[:line.index("=")]
                 key2 = line[line.index("=")+1:]
                 key1_0, key1_1 = key1.split(".")
-                if not key1_0 in op:
-                    op[key1_0] = {}
-                op[key1_0][key1_1] = key2
+                if not key1_0 in ops:
+                    ops[key1_0] = {}
+                ops[key1_0][key1_1] = key2
 
 
-def check_custom_op_opInfo(required_custom_op_info_keys, op, op_key):
-    """
+def check_custom_op_opinfo(required_custom_op_info_keys, ops, op_key):
+    '''
     check custom op info
-    """
-    op_info = op["opInfo"]
-    missing_keys=[]
+    '''
+    op_info = ops["opInfo"]
+    missing_keys = []
     for required_op_info_key in required_custom_op_info_keys:
         if not required_op_info_key in op_info:
             missing_keys.append(required_op_info_key)
@@ -66,99 +66,103 @@ def check_custom_op_opInfo(required_custom_op_info_keys, op, op_key):
         raise KeyError("bad key value")
 
 
-def check_op_opInfo(required_op_info_keys, required_custom_op_info_keys, op, op_key):
-    """
+def check_op_opinfo(required_op_info_keys, required_custom_op_info_keys,
+                    ops, op_key):
+    '''
     check normal op info
-    """
-    op_info = op["opInfo"]
-    missing_keys=[]
+    '''
+    op_info = ops["opInfo"]
+    missing_keys = []
     for required_op_info_key in required_op_info_keys:
         if not required_op_info_key in op_info:
             missing_keys.append(required_op_info_key)
     if len(missing_keys) > 0:
         print("op: " + op_key + " opInfo missing: " + ",".join(missing_keys))
         raise KeyError("bad key value")
-    elif op_info["opKernelLib"] == "CUSTAICPUKernel":
-        check_custom_op_opInfo(required_custom_op_info_keys, op, op_key)
-        op["opInfo"]["userDefined"] = "True"
+    if op_info["opKernelLib"] == "CUSTAICPUKernel":
+        check_custom_op_opinfo(required_custom_op_info_keys, ops, op_key)
+        ops["opInfo"]["userDefined"] = "True"
 
 
-def check_op_input_output(info, key, op):
-    """
+def check_op_input_output(info, key, ops):
+    '''
     check input and output infos of all ops
-    """
-    for op_sets in op[key]:
-        if (op_sets not in ('format', 'type', 'name')):
+    '''
+    for op_sets in ops[key]:
+        if op_sets not in ('format', 'type', 'name'):
             print(info + " should has format type or name as the key, "
-                    + "but getting " + op_sets)
+                  + "but getting " + op_sets)
             raise KeyError("bad op_sets key")
 
 
 def check_op_info(aicpu_ops):
-    """
+    '''
     check all ops
-    """
+    '''
     print("==============check valid for aicpu ops info start==============")
     required_op_info_keys = ["computeCost", "engine", "flagAsync", "flagPartial", "opKernelLib"]
     required_custom_op_info_keys = ["kernelSo", "functionName", "workspaceSize"]
+
     for op_key in aicpu_ops:
-        op = aicpu_ops[op_key]
-        for key in op:
+        ops = aicpu_ops[op_key]
+        for key in ops:
             if key == "opInfo":
-                check_op_opInfo(required_op_info_keys, required_custom_op_info_keys, op, op_key)
+                check_op_opinfo(required_op_info_keys,
+                                required_custom_op_info_keys, ops, op_key)
 
             elif (key[:5] == "input") and (key[5:].isdigit()):
-                check_op_input_output("input", key, op)
+                check_op_input_output("input", key, ops)
             elif (key[:6] == "output") and (key[6:].isdigit()):
-                check_op_input_output("output", key, op)
+                check_op_input_output("output", key, ops)
             elif (key[:13] == "dynamic_input") and (key[13:].isdigit()):
-                check_op_input_output("dynamic_input", key, op)
+                check_op_input_output("dynamic_input", key, ops)
             elif (key[:14] == "dynamic_output") and (key[14:].isdigit()):
-                check_op_input_output("dynamic_output", key, op)
+                check_op_input_output("dynamic_output", key, ops)
             else:
                 print("Only opInfo, input[0-9], output[0-9] can be used as a key, "
-                      "but op %s has the key %s" %(op_key, key))
+                      "but op %s has the key %s" % (op_key, key))
                 raise KeyError("bad key value")
     print("==============check valid for aicpu ops info end================\n")
 
 
 def write_json_file(aicpu_ops_info, json_file_path):
-    """
+    '''
     write json file from ini file
-    """
+    '''
     json_file_real_path = os.path.realpath(json_file_path)
-    with open(json_file_real_path, "w") as f:
+    with open(json_file_real_path, "w") as json_file:
         # Only the owner and group have rights
         os.chmod(json_file_real_path, stat.S_IWGRP + stat.S_IWUSR + stat.S_IRGRP + stat.S_IRUSR)
-        json.dump(aicpu_ops_info, f, sort_keys=True, indent=4, separators=(',', ':'))
+        json.dump(aicpu_ops_info, json_file, sort_keys=True,
+                  indent=4, separators=(',', ':'))
     print("Compile aicpu op info cfg successfully.")
 
 
-def parse_ini_to_json(ini_file_path, outfile_path):
-    """
+def parse_ini_to_json(ini_file_paths_arg, outfile_path_arg):
+    '''
     parse ini to json
-    """
-    aicpu_ops_info = parse_ini_files(ini_file_path)
+    '''
+    aicpu_ops_info = parse_ini_files(ini_file_paths_arg)
     try:
         check_op_info(aicpu_ops_info)
-        write_json_file(aicpu_ops_info, outfile_path)
+        write_json_file(aicpu_ops_info, outfile_path_arg)
     except KeyError:
         print("bad format key value, failed to generate json file")
 
 
 if __name__ == '__main__':
-    args = sys.argv
+    get_args = sys.argv
 
-    outfile_paths = "tf_kernel.json"
+    OUTPUT = "tf_kernel.json"
     ini_file_paths = []
 
-    for arg in args:
+    for arg in get_args:
         if arg.endswith("ini"):
             ini_file_paths.append(arg)
         if arg.endswith("json"):
-            outfile_paths = arg
+            OUTPUT = arg
 
     if len(ini_file_paths) == 0:
         ini_file_paths.append("tf_kernel.ini")
 
-    parse_ini_to_json(ini_file_paths, outfile_paths)
+    parse_ini_to_json(ini_file_paths, OUTPUT)
--- cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/insert_op_info.py
+++ cplusplus/level1_single_api/4_op_dev/1_custom_op/cmake/util/insert_op_info.py
@@ -14,20 +14,20 @@ if __name__ == '__main__':
         exit(2)
 
     with open(sys.argv[1], 'r') as load_f:
-        insert_op = json.load(load_f)
+        insert_operator = json.load(load_f)
 
-    all_op = {}
+    all_operators = {}
     if os.path.exists(sys.argv[2]):
         if os.path.getsize(sys.argv[2]) != 0:
             with open(sys.argv[2], 'r') as load_f:
-                all_op = json.load(load_f)
+                all_operators = json.load(load_f)
 
-    for k in insert_op.keys():
-        if k in all_op.keys():
+    for k in insert_operator.keys():
+        if k in all_operators.keys():
             print('replace op:[', k, '] success')
         else:
             print('insert op:[', k, '] success')
-        all_op[k] = insert_op[k]
+        all_operators[k] = insert_operator[k]
 
-    with open(sys.argv[2], 'w') as f:
-        f.write(json.dumps(all_op, indent=4))
+    with open(sys.argv[2], 'w') as json_file:
+        json_file.write(json.dumps(all_operators, indent=4))
